16d8d19fac4fa8dcd507e0f9b58f4244a0a10387
Tsuzumi Sato <tsuzumisato@gmail.com>
2018-08-08 15:41:03 +0900

    Commiting before yoga

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '12 34 56 78 90',
+    category:     'chinese'
+  },
+  {
+    name:         'Popeyes',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '12 34 56 78 90',
+    category:     'italian'
+  },
+  {
+    name:         'Mcdonalds',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '12 34 56 78 90',
+    category:     'italian'
+  },
+  {
+    name:         'Nobu',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '12 34 56 78 90',
+    category:     'japanese'
+  },
+  {
+    name:         'Joel Robuchon',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '12 34 56 78 90',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
